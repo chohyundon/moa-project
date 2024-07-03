864af94bed177e96cc7547cbe92bf3294ca4f2d1
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-07-03 16:18:21 +0900

    style : primary 컬러 텍스트 설정 및 로고 이미지 추가

---
+++ style/style.ts
@@ -0,0 +1,35 @@
+export const Colors = {
+  MoaGray: {
+    primaryWhite: "#FFFFFF",
+    gray50: "#FCFCFC",
+    gray100: "#F8F8F8",
+    gray200: "#EFEFEF",
+    gray300: "#DFDFDF",
+    gray400: "#B7B7B7",
+  },
+  MoaCarrot: {
+    carrot100: "#FFEAD5",
+    carrot200: "#FDDCAB",
+    carrot300: "#FEB173",
+    carrot400: "#FD853A",
+    carrot500: "#FB6514",
+    carrot600: "#EC4A0A",
+    carrot700: "#C4320A",
+    carrot800: "#9C2A10",
+    carrot900: "#7E2410",
+  },
+};
+
+export const Size = {
+  heading1ExtraBold32: 32,
+  heading2Bold28: 28,
+  heading3Bold24: 24,
+  title1Bold20: 20,
+  title2Bold18: 18,
+  title3Bold16: 16,
+  body1Medium18: 18,
+  body2Medium16: 16,
+  body3Medium14: 14,
+  body4Medium12: 12,
+  caption1Regular12: 12,
+};
