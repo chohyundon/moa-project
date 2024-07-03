621c07679de09cbc5ae94e83a0925d5886b0aec8
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-07-03 16:06:07 +0900

    feature : 라우팅 설정정

--- App.tsx
+++ App.tsx
@@ -1,20 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import Home from "./screens/Home";
+import MyPage from "./screens/Mypage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Coupon from "./screens/Coupon";
+import Share from "./screens/Share";
+import { WithLocalSvg } from "react-native-svg/css";
 
-export default function App() {
+const BottomTab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+export function Bottom() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+      }}
+    >
+      <BottomTab.Screen
+        name="홈"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, size }) =>
+            focused ? (
+              <WithLocalSvg
+                asset={require("./assets/images/ActiveHome.svg")}
+                width={size}
+                height={size}
+              />
+            ) : (
+              <WithLocalSvg
+                asset={require("./assets/images/home.svg")}
+                width={size}
+                height={size}
+              />
+            ),
+        }}
+      />
+      <BottomTab.Screen
+        name="쿠폰"
+        component={Coupon}
+        options={{
+          tabBarIcon: ({ focused, size }) =>
+            focused ? (
+              <WithLocalSvg
+                asset={require("./assets/images/ActiveCoupon.svg")}
+                width={size}
+                height={size}
+              />
+            ) : (
+              <WithLocalSvg
+                asset={require("./assets/images/coupon.svg")}
+                width={size}
+                height={size}
+              />
+            ),
+        }}
+      />
+      <BottomTab.Screen
+        name="공유"
+        component={Share}
+        options={{
+          tabBarIcon: ({ focused, size }) =>
+            focused ? (
+              <WithLocalSvg
+                asset={require("./assets/images/ActiveShare.svg")}
+                width={size}
+                height={size}
+              />
+            ) : (
+              <WithLocalSvg
+                asset={require("./assets/images/share.svg")}
+                width={size}
+                height={size}
+              />
+            ),
+        }}
+      />
+      <BottomTab.Screen
+        name="MY"
+        component={MyPage}
+        options={{
+          tabBarIcon: ({ focused, size }) =>
+            focused ? (
+              <WithLocalSvg
+                asset={require("./assets/images/ActiveMypage.svg")}
+                width={size}
+                height={size}
+              />
+            ) : (
+              <WithLocalSvg
+                asset={require("./assets/images/mypage.svg")}
+                width={size}
+                height={size}
+              />
+            ),
+        }}
+      />
+    </BottomTab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Bottom"
+            component={Bottom}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
